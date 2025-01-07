56417692baf7037999fd718ab197131decfecfc2
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-01-07 18:12:00 +0530

    User service

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,8 @@
 dnf install nginx -y
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl restart nginx
 systemctl enable nginx
+systemctl restart nginx
