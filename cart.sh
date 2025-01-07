1ef7bfaa2d65d96a1e2d18c3f3183156eaadd76d
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-01-07 18:48:13 +0530

    Add Cart Buy

--- cart.sh
+++ cart.sh
@@ -1,11 +1,39 @@
+echo -e "\e[36m>>>>>>>>> Configuring NodeJs repo <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+
+echo -e "\e[36m>>>>>>>>> Install NodeJs <<<<<<<<<\e[0m"
+dnf install nodejs -y
+
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Download APP Content <<<<<<<<<\e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<<\e[0m"
 unzip /tmp/cart.zip
+
+echo -e "\e[36m>>>>>>>>> Install Nodejs Dependecies <<<<<<<<<\e[0m"
 npm install
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>> Start Create Application Directory <<<<<<<<<\e[0m"
+cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>> Start Cart Service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
+
+echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<<\e[0m"
+cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>> Install MongoDB client <<<<<<<<<\e[0m"
+dnf install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<<\e[0m"
+mongo --host mongodb.saikumar22.store </app/schema/user.js
