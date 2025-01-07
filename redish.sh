dc0821b47b719e8117dc8b0cef770cda415a85fa
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-01-07 14:16:46 +0530

    Adding paths

---
+++ redish.sh
@@ -0,0 +1,16 @@
+echo -e "\e[36m>>>>>>>>> Install Redis <<<<<<<<<\e[0m"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>>>> Install Redis <<<<<<<<<\e[0m"
+dnf module enable redis:remi-6.2 -y
+yum install redis -y
+
+echo -e "\e[36m>>>>>>>>> Update Redis Listen Adress <<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
+echo -e "\e[36m>>>>>>>>> Start Redis Service <<<<<<<<<\e[0m"
+systemctl enable redis
+systemctl start redis
+
+
+
