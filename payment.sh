7f585a046e8ed60a460b03c7301897c9964f7645
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-01-08 20:09:33 +0530

    payment service

--- payment.sh
+++ payment.sh
@@ -1,13 +1,27 @@
+echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<<\e[0m"
 dnf install python36 gcc python3-devel -y
+
+echo -e "\e[36m>>>>>>>>> App Application User <<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create App Directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e "\e[36m>>>>>>>>> Extract App Content <<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/payment.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
-cp payment.service /etc/systemd/system/payment.service
+echo -e "\e[36m>>>>>>>>> Setup Systemd Service <<<<<<<<<\e[0m"
+cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
+
+echo -e "\e[36m>>>>>>>>> Start Payment Service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
