cea18e561e87d8bbd1c0ddd4ceb063ffe72d6b0f
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-03 18:22:38 +0530

    source common

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,6 @@
+script_path=$(dirname $0)
+source ${script_path}|common.sh
+
 echo -e "\e[36m>>>>>>>>> Disable MySQL Version <<<<<<<<<\e[0m"
 dnf module disable mysql -y
 
--- payment.sh
+++ payment.sh
@@ -1,4 +1,5 @@
-source common.sh
+script_path=$(dirname $0)
+source ${script_path}|common.sh
 
 echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<<\e[0m"
 dnf install python36 gcc python3-devel -y
