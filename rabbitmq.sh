c87420c43cda81f961ee8ce6c3b3a2e963faef9b
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-09 11:59:56 +0530

    all topic shell script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -9,25 +9,25 @@ if [ -z "$rabbitmq_appuser_password"]; then
 fi
 
 func_print_head "Setup Erlang Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash $<<log_files
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash $<<log_file
 
 func_stat_check $?
 
 func_print_head "Setup Rabbitmq Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash $<<log_files
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash $<<log_file
 func_stat_check $?
 
 func_print_head "Install Erlang RabbitMQ"
-dnf install erlang rabbitmq-server -y $<<log_files
+dnf install erlang rabbitmq-server -y $<<log_file
 func_stat_check $?
 
 func_print_head "Start RabbitMQ Service"
-systemctl enable rabbitmq-server $<<log_files
-systemctl restart rabbitmq-server $<<log_files
+systemctl enable rabbitmq-server $<<log_file
+systemctl restart rabbitmq-server $<<log_file
 func_stat_check $?
 
 
 func_print_head "Add Application User In RabbitMQ"
-rabbitmqctl add_user roboshop roboshop123 $<<log_files
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" $<<log_files
+rabbitmqctl add_user roboshop roboshop123 $<<log_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" $<<log_file
 func_stat_check $?
