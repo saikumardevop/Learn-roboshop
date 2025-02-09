bae8253ce38bc1fc5f91630ccd75de01860666f3
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-09 11:31:13 +0530

    all topic shell script

--- frontend.sh
+++ frontend.sh
@@ -4,27 +4,27 @@ source ${script_path}/common.sh
 
 
 func_print_head "Install Nginx"
-yum install nginx -y $>>log file
+yum install nginx -y $>>$log_file
 func_stat_check $?
 
 func_print_head "Copy roboshop Config file"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf $>>$log file
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf $>>$log_file
 func_stat_check $?
 
 func_print_head "Clean Old App Content"
-rm -rf /usr/share/nginx/html/* $>>$log file
+rm -rf /usr/share/nginx/html/* $>>$log_file
 func_stat_check $?
 
 func_print_head "Download App Content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip $>>$log file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip $>>$log_file
 func_stat_check $?
 
 func_print_head "Extracting APP Content"
-cd /usr/share/nginx/html $>>$log file
-unzip /tmp/frontend.zip $>>$log file
+cd /usr/share/nginx/html $>>$log_file
+unzip /tmp/frontend.zip $>>$log_file
 func_stat_check $?
 
 func_print_head "Start Nginx"
-systemctl enable nginx $>>$log file
-systemctl restart nginx $>>$log file
+systemctl enable nginx $>>$log_file
+systemctl restart nginx $>>$log_file
 func_stat_check $?
