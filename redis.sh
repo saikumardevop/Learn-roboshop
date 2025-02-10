a6fc11aac16407347644a5cf729b891edb6d2dfd
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-10 16:01:07 +0530

    all topic shell script

--- redis.sh
+++ redis.sh
@@ -3,21 +3,21 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 func_print_head "Install Redis Repos"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$log_file
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
 func_stat_check $?
 
 func_print_head "Install Redis"
-dnf module enable redis: remi-6.2 -y &>>$log_file
-yum install redis -y &>>$log_file
+dnf module enable redis: remi-6.2 -y
+yum install redis -y 
 func_stat_check $?
 
 func_print_head "Update Redis Listen Address"
-sed -i -e 's|127.0.0.1|0.0.0.0|' etc/redis.conf /etc/redis/redis.conf &>>$log_file
+sed -i -e 's|127.0.0.1|0.0.0.0|' etc/redis.conf /etc/redis/redis.conf 
 func_stat_check $?
 
 func_print_head "Start Redis Service"
-systemctl enable redis &>>$log_file
-systemctl restart redis &>>$log_file
+systemctl enable redis 
+systemctl restart redis 
 func_stat_check $?
 
 
