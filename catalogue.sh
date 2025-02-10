9479085a60c0b09fdd6a9fcca00deff01dbd262e
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-10 15:03:30 +0530

    all topic shell script

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 
 component=catalogue
 schema_setup=mango
-func_nodes
+func_node
 
 echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<<\e[0m"
 cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
@@ -13,4 +13,4 @@ echo -e "\e[36m>>>>>>>>> Install MongoDB client <<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
 
 echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<<\e[0m"
-mongo --host mongodb.saikumar22.store </app/schema/user.js
+mongo --host mongodb.saikumar22.store </app/schema/catalogue.js
